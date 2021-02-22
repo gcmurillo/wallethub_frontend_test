3938ea6cd23da0c8c92b1c92da15a720ac1c5c02
gcmurillo <gcmurillo@outlook.com>
2021-02-21 22:23:45 -0500

    ADD Test04

--- src/app/test-04.ts
+++ src/app/test-04.ts
@@ -3,14 +3,18 @@
  * First name and last name should be lowercased, and then a random integer between 1 and 9 should be added to the end
  * For example: if the inputs are "John" and "DOE" the generated username could be "john_doe_4" or "john_doe_2"
  */
-import { Component, NgModule  } from '@angular/core';
+import { Component, NgModule, ElementRef, HostListener, Host } from '@angular/core';
 import { RouterModule} from "@angular/router";
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector : 'ng-app',
     template : `
                 <h2>Enter your first and last name</h2>
+                First name: <input type="text" [(ngModel)]="first_name" /> <br/>
+                Last name: <input type="text" [(ngModel)]="last_name" /> <br/>
+                Username: {{ username }}
                 <div>
                 </div>
                 `,
@@ -18,6 +22,25 @@ import { CommonModule } from '@angular/common';
 })
 export class UserNameComponent {
     
+    first_name: string = "";
+    last_name: string = "";
+    username: string = "";
+
+    @HostListener('document:click', ['$event'])
+    clickout(e) {
+        if (!this.eRef.nativeElement.contains(e.target)) {
+            console.log("OUTSIDE");
+            if (this.first_name.replace(/\s/g, "") != "" && this.last_name.replace(/\s/g, "") != "") {
+                this.username = this.first_name.toLocaleLowerCase() + "_" + this.last_name.toLocaleLowerCase() + "_" + this.getRandomInt(1, 9);
+            }
+        } 
+    }
+
+    getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    constructor(private eRef: ElementRef) {} 
 
 }
 
@@ -29,7 +52,9 @@ export class UserNameComponent {
                 path : "",
                 component : UserNameComponent
             }
-        ])
+        ]),
+        FormsModule,
+        ReactiveFormsModule
     ],
     declarations : [UserNameComponent]
 })
